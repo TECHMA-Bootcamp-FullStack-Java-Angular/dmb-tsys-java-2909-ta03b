c6134647ab31434552fce3e514a562e16730a80f
David Maza Borjabad <109163247+d-maza@users.noreply.github.com>
2023-10-01 20:20:04 +0200

    add index pages for better navigation

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
-#dmb-tsys-java-2909
+<div align="center"><img width="80%"  src="https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-java-2909-ta03a/blob/main/utils/BootCamp-reduced.gif?raw=true"  alt="Logo BootCamp" /></div>
+<hr>
 
-Tarea 03 de la Unidad 1 del Bootcamp Techma
+![My etiqueta](https://img.shields.io/badge/David%20Maza-DiveCode%F0%9F%90%99-blue) ![UD](https://img.shields.io/badge/TA-3b-orange)  ![Java Version](https://img.shields.io/badge/java-v8-red) ![GitHub repo size](https://img.shields.io/github/repo-size/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-java-2909-ta03b) ![GitHub repo licence](https://img.shields.io/github/license/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-java-2909-ta03b) 
+
+ Este repositorio contiene las tareas del Bootcamp 👨‍💻
+
+
+Puedes acceder a cada uno de los ejercicios haciendo clic en los enlaces siguientes:
+
+
+- [Ejercicio 1](https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-java-2909-ta03b/blob/main/src/com/tmbs/ta02/ClasseInteger1.java) -> Diferentes Constructores ⚒️
+
+- [Ejercicio 2](https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-java-2909-ta03b/blob/main/src/com/tmbs/ta02/ComentarioLinea.java) -> Comentario // 📝
+
+- [Ejercicio 3](https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-java-2909-ta03b/blob/main/src/com/tmbs/ta02/ComentarioVariasLineas.java) -> Comentario en varias lineas /* ... */ 🗒️ 
+
+- [Ejercicio 4](https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-java-2909-ta03b/blob/main/src/com/tmbs/ta02/HolaMundo.java) -> Hola Mundo 🌎
+  
---
+++ index/js/lessons.js
@@ -0,0 +1,58 @@
+export const lessons = [
+  {
+    numLessons: 1,
+    issue: 'Java SE',
+    day: 3,
+    msg: '<i> Completamente entusiamado </i> 🤩',
+    myProgress: '🥚',
+
+    exercise: [
+      {
+        numExercise: 1,
+        title: '',
+        description:`
+        <li>Creación de un objeto Integer usando el <b> segundo </b> constructor. </li>
+        <li>Creación de un objeto Integer usando el <b> primer </b> constructor.</li>
+        <li>Obtención del entero que almacena cada objeto Integer.</li>
+        <li>Si no se hace esto, las lineas siguientes causarian error de compilación.</li>`,
+        urlPG: 'https://www.jdoodle.com/iembed/v0/NhU',
+        urlCode: 'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-java-2909-ta03b/blob/main/src/com/tmbs/ta02/ClasseInteger1.java'
+      },
+      {
+        numExercise: 2,
+        title: '',
+        description: 'Genera un comentario en linea //🧐',
+        urlCode: 'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-java-2909-ta03b/blob/main/src/com/tmbs/ta02/ComentarioLinea.java',
+        urlPG: 'https://www.jdoodle.com/iembed/v0/Njb'
+      },
+      {
+        numExercise: 3,
+        title: '',
+        description: 'Genera un comentario en varias lineas 📝',
+        urlCode: 'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-java-2909-ta03b/blob/main/src/com/tmbs/ta02/ComentarioVariasLineas.java',
+        urlPG: 'https://www.jdoodle.com/iembed/v0/Njc'
+      },
+      {
+        numExercise: 4,
+        title: '',
+        description: 'Genera un Hola Mundo que se vea por consola 👋🏼🌍',
+        urlCode: 'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-java-2909-ta03b/blob/main/src/com/tmbs/ta02/HolaMundo.java',
+        urlPG: 'https://www.jdoodle.com/iembed/v0/Njd'
+      },
+      {
+        numExercise: 5,
+        title: '',
+        description:`
+        <li>Muestra un pantallazo con la instalacion de una de estas aplicaciones:</li>
+        <li>GitHub Desktop.</li>
+        <li>Sourcetree.</li>
+        <li>Git Kraken.</li>`, 
+        urlCode: 'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-java-2909-ta03b/blob/main/README.md',
+        urlPG: 'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-java-2909-ta03b/blob/main/src/com/tmbs/ta02/screenshot.png'
+      },
+    ]
+  },
+]
+
+
+ 
